3e04d46837f4d419a6b486d1422bab22873d5088
BalthazarGrimaldi <balthazar.grimaldi@gmail.com>
2019-04-17 14:20:56 +0200

    voilà mon portfolio

---
+++ view/homePage.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../public/css/style.css">
+<body>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm text-center fond-couleur">
+            <img class="img-fluid image-entete" src="../public/image/code_php.png">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6 text-center">
+            <div class="card titre">
+                <div class="card-body">
+                    <h1 class="card-title">Balthazar Grimaldi</h1>
+                    <h4 class="card-text description">Jeune dynamique de 23 ans, en première année de BTS SIO SLAM,
+                        recherche un
+                        stage en développement informatique</h4>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3"></div>
+    </div>
+    <div class="row nav-perso">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-2">
+            <a href="#accueil">
+                <button type="button" class="btn btn-outline-dark mes-boutons"><i class="fas fa-user taille-icone"></i>
+                    Accueil
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <a href="#mon-ecole">
+                <button type="button" class="btn btn-outline-dark mes-boutons"><i
+                            class="fas fa-university taille-icone"></i> Mon école
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <a href="#mes-projets">
+                <button type="button" class="btn btn-outline-dark mes-boutons"><i
+                            class="fas fa-project-diagram taille-icone"></i> Mes projets
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <a href="#mes-cours">
+                <button type="button" class="btn btn-outline-dark mes-boutons"><i
+                            class="fas fa-file-pdf taille-icone"></i> Mes cours
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <a href="#contact">
+                <button type="button" class="btn btn-outline-dark mes-boutons"><i
+                            class="fas fa-envelope taille-icone"></i> Contact
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div id="accueil" class="row presentation">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-4">
+            <img class="ma_photo" src="../public/image/ma_photo.jpg" alt="ma photo">
+        </div>
+        <div class="col-sm-6">
+            <p class="texte_pre">
+                <br> <br>
+                Je suis en première année de BTS SIO (services informatiques aux organisations) option SLAM
+                (Solutions logicielles et applications métiers). J'étudie ainsi le dévellopement informatique et
+                je recherche un stage pour l'année scolaire 2018-2019 ainsi qu'une alternance pour l'année
+                scolaire 2019-2020.
+                <br> <br>
+                Passionné par l'intelligence artificielle, je suis en cours de ré-orientation après une année en
+                Histoire-Géographie à la faculté Sorbonne-Université ainsi qu'une année en PGCI (Physique Chimie
+                Géoscience Ingénierie) à l'Université Pierre et Marie Curie
+                <br> <br>
+                Titulaire d'un Bac S, j'ai pratiqué la boxe et la vélo et je prends actuellement des cours de théatre
+                dans une troupe, avec représentations en fin d'année.
+            </p>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <embed src="../public/pdf/CV.pdf#" type="application/pdf" width="100%" height="600px"/>
+        </div>
+        <div class="col-sm-2"></div>
+    </div>
+    <div class="titres">
+        <h2 id="mon-ecole">Mon Ecole</h2>
+    </div>
+    <div class="row photos_cfa">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-5">
+            <img class="cfa" src="../public/image/cfa.jpg" alt="Photo de l'intérieur du CFA">
+        </div>
+        <div class="col-sm-5">
+            <img class="cfabis" src="../public/image/cfabis.jpg" alt="Photo d'une salle de classe">
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <p class="description_ecole">
+                Le CFA est une école qui propose des formations de Bac + 2 à Bac + 5 en informatique (aussi bien en
+                réseau qu'en dévelopement) ainsi qu'en comptabilité et ressources humaines.
+                <br> <br>
+                Comme tous les CFA (centres de formation d'apprentis), le CFA INSTA est un établissement de formation
+                assurant un enseignement en alternance à des apprentis. Toutefois dans le cas de CFA INSTA, avoir une
+                alternance pour son année n'est pas obligatoire à condition d'avoir un stage non rémunéré pendant
+                l'année de 6 semaines pleines. C'est à dire dans mon cas, 8 à 10 semaines de trois jours: le mercredi,
+                le jeudi et le vendredi. Les cours étant dispensés le lundi et le mardi en ce qui concerne la première
+                année du BTS SIO option SLAM.
+                <br> <br>
+                En plus des enseignants, l'école dispose d'encadrants diplomés dans les différentes formations que
+                propose l'école. Ils peuvent nous aider si nous rencontrons un problème à résoudre dans la cadre par
+                exemple d'un devoir à rendre ou d'un projet à faire en entreprise.
+            </p>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="titres">
+        <h2 id="mes-projets">Mes projets</h2>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <h3 class="premiere_ligne">Mon premier PPE</h3>
+            <embed src="../public/pdf/PPE.pdf#" type="application/pdf" width="100%" height="800px"/>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <h3 class="autres_ligne">Mon deuxième PPE</h3>
+            <embed src="../public/pdf/PPE2.pdf#" type="application/pdf" width="100%" height="800px"/>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+
+    <div class="titres">
+        <h2 id="mes-cours">Mes cours</h2>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <h3 class="premiere_ligne">Développement informatique</h3>
+            <embed src="../public/pdf/CV.pdf#" type="application/pdf" width="100%" height="800px"/>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <h3 class="autres_ligne">Mathématique</h3>
+            <embed src="../public/pdf/CV.pdf#" type="application/pdf" width="100%" height="800px"/>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <h3 class="autres_ligne">Economie, droit et management</h3>
+            <embed src="../public/pdf/CV.pdf#" type="application/pdf" width="100%" height="800px"/>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <h3 class="autres_ligne">Anglais</h3>
+            <embed src="../public/pdf/CV.pdf#" type="application/pdf" width="100%" height="800px"/>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
+            <h3 class="autres_ligne">Culture générale</h3>
+            <embed src="../public/pdf/CV.pdf#" type="application/pdf" width="100%" height="800px"/>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+
+    <div class="titres">
+        <h2 id="contact">Contact</h2>
+    </div>
+   <div class="row">
+       <div class="col-sm-2"></div>
+       <div class="col-sm-8">
+           <form>
+               <div class="form-group">
+                   <label for="">Nom</label>
+                   <input type="text" class="form-control" id="nom" placeholder="Nom">
+               </div>
+               <div class="form-group">
+                   <label for="">Prenom</label>
+                   <input type="text" class="form-control" id="prenom" placeholder="Prenom">
+               </div>
+               <div class="form-group">
+                   <label for="">Email</label>
+                   <input type="email" class="form-control" id="email" placeholder="Email">
+               </div>
+               <div class="form-group">
+                   <label for="">Message</label>
+                   <textarea class="form-control" id="message" rows="3"></textarea>
+               </div>
+               <button type="submit" class="btn btn-primary mb-2">Envoyez !</button>
+           </form>
+       </div>
+       <div class="col-sm-2"></div>
+   </div>
+
+</div>
+
+<script src="../public/bootstrap/js/bootstrap.min.js"></script>
+<script src="../public/fontawesome/js/all.min.js"></script>
+
+</body>
+</html>
